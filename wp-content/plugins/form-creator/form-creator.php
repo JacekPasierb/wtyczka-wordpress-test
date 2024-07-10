47d271b277105318a266ee67692805280c67c7b3
Jacek Pasierb <jack_byk@o2.pl>
2024-07-10 16:17:19 +0200

    Feature/php - clear code

--- wp-content/plugins/form-creator/form-creator.php
+++ wp-content/plugins/form-creator/form-creator.php
@@ -23,14 +23,4 @@ require_once plugin_dir_path(__FILE__) . 'includes/settings.php';
 require_once plugin_dir_path(__FILE__) . 'includes/payu-ajax.php';
 require_once plugin_dir_path(__FILE__) . 'includes/thank-you-page.php';
 require_once plugin_dir_path(__FILE__) . 'includes/payu-notify.php';
-
-// Dodaj skrypt JavaScript do obsługi AJAX
-function form_creator_enqueue_scripts() {
-    wp_enqueue_script('form-creator-ajax', plugin_dir_url(__FILE__) . 'form-creator-ajax.js', ['jquery'], null, true);
-
-    wp_localize_script('form-creator-ajax', 'formCreatorAjax', [
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'nonce' => wp_create_nonce('your_nonce_action')
-    ]);
-}
-add_action('wp_enqueue_scripts', 'form_creator_enqueue_scripts');
+require_once plugin_dir_path(__FILE__) . 'includes/scripts.php';
---
+++ wp-content/plugins/form-creator/includes/scripts.php
@@ -0,0 +1,11 @@
+<?php
+function form_creator_enqueue_scripts() {
+    wp_enqueue_script('form-creator-ajax', plugin_dir_url(__FILE__) . 'form-creator-ajax.js', ['jquery'], null, true);
+
+    wp_localize_script('form-creator-ajax', 'formCreatorAjax', [
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('your_nonce_action')
+    ]);
+}
+add_action('wp_enqueue_scripts', 'form_creator_enqueue_scripts');
+?>
